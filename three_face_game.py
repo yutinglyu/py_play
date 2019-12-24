f36373051fcde019c1b86a845c40c8675f94a8bb
Yuting Lyu <yutinglv1994@163.com>
2019-12-24 17:14:21 +0800

    thinking how to clean cards and wager and so on.

--- three_face_game.py
+++ three_face_game.py
@@ -20,31 +20,36 @@ Created on Fri Dec 20 14:56:54 2019
              6.0点、1点平则庄家赢
 @author: user
 """
+import random
 
 
 class Player:
     """
     定义一个玩家
     """
-    def __init__(self, funds, bottom_wager, is_bookmaker = False):
+    def __init__(self, name, seat_index, funds, bottom_wager, 
+                 is_bookmaker = False):
+        self.name = name
+        self.seat_index = seat_index
         self.funds = funds
         self.bottom_wager = bottom_wager
         self.is_bookmaker = is_bookmaker
     # 属性
     is_bookmaker = False
+    name = ''
+    seat_index = 0
     funds = 0
     bottom_wager = 0
-    card_list = []    
-    # 自我评估手牌大小
-    def assess_cards():
-        pass    
-    # 下注
-    def put_wager():
-        pass
+    self_cards = []   
+    wager = 0
+    # 自我评估手牌大小，下注
+    def assess_put_wager(self):
+        self.wager = self.bottom_wager
+        return self.wager
     # 切牌
-    def cut_card():
-        pass
-    
+    def cut_card(self, pokerDealer_obj):
+        choosed_number = random.sample(pokerDealer_obj.card_list, 1)[0][1]
+        return (self.seat_index + choosed_number) % pokerDealer_obj.number_of_player    
     
 class PokerDealer:
     """
@@ -57,6 +62,7 @@ class PokerDealer:
         single_cards = [(i+'_'+j) for i in suits for j in ranks]
         self.card_list = list(zip(single_cards,list(range(1,14))*4))
         self.seats_order = seats_order
+        self.bookmaker_name = seats_order[0]
         if if_joker:
             self.card_list.append(('Black_Joker',0))
             self.card_list.append(('Red_Joker',0))   
@@ -65,17 +71,26 @@ class PokerDealer:
     number_of_player = 4
     card_list = []
     seats_order = []
+    bookmaker_name = ''
     bookmaker_index = 0
     # 切牌选庄
     def choose_bookmaker(self):
-        
-        pass
+        choosed_start_seat = random.randint(0, self.number_of_player-1)
+        choosed_number = random.sample(self.card_list, 1)[0][1]
+        self.bookmaker_index = (choosed_start_seat + choosed_number) % self.number_of_player
+        self.bookmaker_name = self.seats_order[self.bookmaker_index]
+        return (self.bookmaker_name, self.bookmaker_index)
     # 发牌
-    def distribute_cards():
-        pass    
+    def distribute_cards(self):
+        if len(self.card_list) < 3 * self.number_of_player:
+            return []
+        drawn_cards_list = random.sample(self.card_list, 3 * self.number_of_player)
+        self.card_list = [x for x in self.card_list if x not in drawn_cards_list]
+        return [drawn_cards_list[i:i+3] for i in range(0,len(drawn_cards_list),3)]
     #　比较
+    @staticmethod
     def compare_cards(self_cards, oppo_cards):
-        return True    
+        return 'win' 
     # 二人结算
     def deal_2player():
         pass    
@@ -86,11 +101,32 @@ class PokerDealer:
 
 
 def main():
-    pokerDealer = PokerDealer(['Tim','Zhu','Lin','Hui'])
-    pokerDealer.choose_bookmaker()
-    print(pokerDealer.bookmaker_index)
+    player1 = Player('Tim', 0, 100, 2)
+    player2 = Player('Zhu', 1, 100, 2)
+    player3 = Player('Lin', 2, 100, 2)
+    player4 = Player('Hui', 3, 100, 2)
+    player_list = [player1, player2, player3, player4]
+    pokerDealer = PokerDealer([player1.name, player2.name, player3.name,
+                               player4.name]) # 实例化发牌器
+    # 1.切牌逆时针选庄；
+    bookmaker_info = pokerDealer.choose_bookmaker()
+    player_list[bookmaker_info[1]].is_bookmaker = True
+    # 2.庄家切牌，选择第一个抽牌的玩家；
+    first_draw_index = player_list[pokerDealer.bookmaker_index].cut_card(pokerDealer)
+    # 3.逆时针抽牌，每人一次性抽三张，评估手牌大小，下注；
+    drawn_cards = pokerDealer.distribute_cards()
+#    if drawn_cards == []:
+    for i in range(pokerDealer.number_of_player):
+        final_i = (first_draw_index + i) % pokerDealer.number_of_player
+        player_list[final_i].self_cards = drawn_cards[i]
+        player_list[final_i].assess_put_wager()
+    # 5.亮牌，分别与庄家进行比较（赢、输、平）,结算；
+    # 6.；
+    # 7.选出牌型最大的玩家切牌，选择第一个抽牌的玩家；
+    # 8.从第3步重新开始。
+    # 9.两副牌以后换庄，所有人都做过一次庄为一轮。    
     
-    print(pokerDealer.card_list.index(('spade_6', 6)))
+    
     
     
     
